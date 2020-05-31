7211c82ae910cd8965d3c07715632947058a5986
Kurtkhris <kurt@localhost.localdomain>
2020-05-30 21:18:01 -0700

    My Angular Web app

---
+++ src/app/login/login.component.spec.ts
@@ -0,0 +1,80 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { routingModule } from '../app.routing';
+import { APP_BASE_HREF } from '@angular/common';
+import { TestCommonDeclarations, TestCommonImports , TestAuthServices } from '../_shared/helpers/test.helper';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+
+import { LoginComponent } from './login.component';
+
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  let httpMock: HttpTestingController;
+
+  const userData = {
+    fullname : 'mock fullname',
+    email : 'mock@me.com',
+    username : 'mock@me.com',
+  };
+
+  const mockToken  = 'token_1234567890';
+
+  const loginResponse = {
+    token : mockToken,
+    user : userData
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ...TestCommonDeclarations,
+      ],
+      imports: [
+        ...TestCommonImports,
+        HttpClientTestingModule,
+        routingModule,
+      ],
+      providers : [
+        ...TestAuthServices,
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
+    })
+    .compileComponents();
+
+    httpMock = TestBed.get(HttpTestingController);
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('check username and password formgroup fields', () => {
+    it('loginForm formgroup should have username field', () => {
+      const field = component.loginForm.get('username');
+      expect(field).toBeTruthy();
+    });
+
+    it('loginForm formgroup should have password field', () => {
+      const field = component.loginForm.get('password');
+      expect(field).toBeTruthy();
+    });
+  });
+
+  it('formdata not set, should not be able to submit', () => {
+    component.loginForm.reset();
+    component.onSubmitButtonClicked();
+    expect(component.error).toBeFalsy();
+  });
+
+
+});
